c569b7589007cd2c5ca336e1cef4305aeb79dae2
Austin <jacoba4@rpi.edu>
2019-11-07 17:00:46 -0500

    Fiddling with metronome

---
+++ EGD3/Assets/Scripts/Metronome.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+    private double bpm = 0;
+    public double fakebpm = 140.0F;
+    public float gain = 0.5F;
+    public int signatureHi = 4;
+    public int signatureLo = 4;
+    private double nextTick = 0.0F;
+    private float amp = 0.0F;
+    private float phase = 0.0F;
+    private double sampleRate = 0.0F;
+    private int accent;
+    private bool running = false;
+    private int beats = 0;
+
+
+
+    public GameObject controller;
+    private TestController controllerscript;
+    void Start()
+    {
+        accent = signatureHi;
+        double startTick = AudioSettings.dspTime;
+        sampleRate = AudioSettings.outputSampleRate;
+        nextTick = startTick * sampleRate;
+        running = true;
+        bpm = fakebpm*4;
+        controllerscript = controller.GetComponent<TestController>();
+    }
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        if(!running)
+        {
+            return;
+        }
+
+        double samplesPerTick = sampleRate * 60.0F / bpm * 4.0F / signatureLo;
+        double sample = AudioSettings.dspTime * sampleRate;
+        int datalen = data.Length / channels;
+        int n = 0;
+        while (n < datalen)
+        {
+            float x = gain * amp * Mathf.Sin(phase);
+            int i = 0;
+            while(i < channels)
+            {
+                data[n * channels + i] += x;
+                i++;
+            }
+            while (sample + n >= nextTick)
+            {
+                nextTick += samplesPerTick;
+                amp = 1.0f;
+                if(++accent > signatureHi)
+                {
+                    accent = 1;
+                    amp *= 2.0F;
+                }
+                switch(accent)
+                {
+                    case 1:
+                        beats++;
+                        Debug.Log(beats);
+                        break;
+                    case 2:
+                        Debug.Log("e");
+                        break;
+                    case 3:
+                        Debug.Log("and");
+                        break;
+                    case 4:
+                        Debug.Log("a");
+                        break;
+                }
+                
+                //Debug.Log("Tick: " + accent + "/" + signatureHi);
+                controllerscript.beat();
+            }
+            phase += amp * 0.3F;
+            amp *= 0.993F;
+            n++;
+        }
+    }
+}
---
+++ EGD3/Assets/Scripts/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+    public GameObject spawn;
+    int pos = -5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void beat()
+    {
+        Vector3 spawnpos = new Vector3(pos,0,0);
+        Instantiate(spawn,spawnpos, Quaternion.identity);
+        pos++;
+    }
+}
