4f3cb1c05495b4301fd167f24e0436be8a15d318
SirBrect <Kerrab@rpi.edu>
2019-12-02 17:42:37 -0500

    arduino testing

---
+++ EGD3/Assets/Scenes/test/jump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+
+public class jump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float jumpamount = 70;
+    SerialPort sp = new SerialPort("COM7", 9600);
+    public Rigidbody rb;
+
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 40;
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sp.IsOpen)
+        {
+            jumpBoi(sp.ReadByte());
+            print(sp.ReadByte());
+        }
+    }
+
+    void jumpBoi(int jumped)
+    {
+        if (jumped == 1)
+        {
+            rb.AddForce(transform.up * jumpamount);
+            print("fuck this worrked");
+        }
+    }
+}
